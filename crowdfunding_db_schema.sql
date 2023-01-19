e1a5a7f5ca44fa4f46c60eb30a24c9a722c851dd
Brent-E-50 <122326425+Brent-E-50@users.noreply.github.com>
2023-01-19 15:23:53 -0500

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,5 +1,4 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/dlKkBe
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 SELECT * FROM campaign;
